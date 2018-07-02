bafb1d4e1125ed6350498049ba58b99e7a1882eb
Julian Fragner <40146720140043@litec.ac.at>
2018-07-02 14:20:43 +0200

    changed comments

--- FireApp_Domain/FireEvent.cs
+++ FireApp_Domain/FireEvent.cs
@@ -8,23 +8,23 @@ namespace FireApp.Domain {
     public class FireEvent {
 
        
-        public FireEventId Id { get; set; } //a composite primary key consisting of source and event id
+        public FireEventId Id { get; set; } // a composite primary key consisting of source and event id
 
-        public DateTime TimeStamp { get; set; } //Time when the FireEvent accored
+        public DateTime TimeStamp { get; set; } // Time when the FireEvent accored
 
-        public string TargetId { get; set; } //name of the Fire detector (e.g. MG 13/5)
+        public string TargetId { get; set; } // name of the Fire detector (e.g. MG 13/5)
 
-        public string TargetDescription { get; set; } //Description of the Location/Fire Detector (e.g. Melder Büro)
+        public string TargetDescription { get; set; } // Description of the Location/Fire Detector (e.g. Melder Büro)
         
         public EventTypes EventType { get; set; }
     }
 
 
-    //help class to use a composite primary key 
+    // help class to use a composite primary key 
     public class FireEventId {
-        public int SourceId { get; set; } //Id of the BMA (Brandmeldeanlage)
-        public int EventId { get; set; } //incrementing id of the events which are raised by the BMA
+        public int SourceId { get; set; } // Id of the BMA (Brandmeldeanlage)
+        public int EventId { get; set; } // incrementing id of the events which are raised by the BMA
     }
 
-public enum EventTypes { alarm, disfunction};
+    public enum EventTypes { alarm, disfunction}; // ... todo
 }
