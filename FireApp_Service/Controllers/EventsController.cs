39f6f8db2c416c81a58340259232236681c896ce
Michael Rath <40146720140057@litec.ac.at>
2018-07-05 15:16:11 +0200

    es geht wieda

--- FireApp_Service/Controllers/EventsController.cs
+++ FireApp_Service/Controllers/EventsController.cs
@@ -141,6 +141,18 @@ namespace FireApp.Service.Controllers
         }
 
         /*******************************************************************************************
+         * public Int32[] CountFireEventsByEventTypePerYear(EventTypes eventType, int year)
+         * 
+         * returns an array with the number of FireEvents of the given EventType where each column
+         * represents one month
+         ******************************************************************************************/
+        [HttpGet, Route("typeyear/{eventType}/{year}")]
+        public Int32[] CountFireEventsByEventTypePerYear(EventTypes eventType, int year)
+        {
+            return DatabaseOperations.CountFireEventsByEventTypePerYear(eventType, year);
+        }
+
+        /*******************************************************************************************
          * public IEnumerable<FireEvent> Active()
          * 
          * returns a list with all active FireEvents
