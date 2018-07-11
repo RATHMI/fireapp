985dcea96cc994d12ab101119f29416393acb7bc
Michael Rath <40146720140057@litec.ac.at>
2018-07-11 14:54:14 +0200

    11.07.2018 14:53 added interfaces for ServiceMembers

--- FireApp_Service/Controllers/ServiceMemberController.cs
+++ FireApp_Service/Controllers/ServiceMemberController.cs
@@ -4,11 +4,43 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using FireApp.Domain;
 
 namespace FireApp.Service.Controllers
 {
     [RoutePrefix("service")]
     public class ServiceMemberController : ApiController
     {
+        /// <summary>
+        /// inserts a ServiceMember into the database or updates it if it already exists
+        /// </summary>
+        /// <param name="sm">The ServiceMember you want to insert</param>
+        /// <returns>returns true if ServiceMember was inserted</returns>
+        [HttpPost, Route("upload")]
+        public static bool UpsertServiceMember(ServiceMember sm)
+        {
+            return DatabaseOperations.ServiceMembers.UpsertServiceMember(sm);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>returns a list with all ServiceMembers</returns>
+        [HttpGet, Route("all")]
+        public static ServiceMember[] GetAllServiceMembers()
+        {
+            return DatabaseOperations.ServiceMembers.GetAllServiceMembers().ToArray<ServiceMember>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">The id of the ServiceMember you are looking for</param>
+        /// <returns>returns a ServiceMember with a matching id</returns>
+        [HttpGet, Route("id/{id}")]
+        public static ServiceMember[] GetServiceMemberById(int id)
+        {
+            return DatabaseOperations.ServiceMembers.GetServiceMemberById(id).ToArray<ServiceMember>();
+        }
     }
 }
