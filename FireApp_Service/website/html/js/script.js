6fc6ecc3e6f28595993ee8dcbee13e98569924f5
Michael Rath <40146720140057@litec.ac.at>
2018-07-12 10:32:00 +0200

    12.07.2018 10:31 deleted useless website files

--- FireApp_Service/website/html/js/script.js
+++
@@ -1,9 +0,0 @@
-/*
-function init() {
-    var cnt = document.getElementsByTagName("tr").length;
-    // console.log(cnt);
-    document.getElementById("cnt_rows").innerHTML = (cnt);
-}
-
-window.onload = init;
-*/
