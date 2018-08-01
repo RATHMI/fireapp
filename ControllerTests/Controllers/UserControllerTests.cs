d2fe5b4009e05509366e0994fc17ac740e9ccf8d
Michael Rath <40146720140057@litec.ac.at>
2018-08-01 16:39:35 +0200

    01.08.2018 16:39 added project for unit tests

---
+++ ControllerTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FireApp.Service.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserController
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void UpsertUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpsertBulkTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCsvTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpsertCsvTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AuthenticateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllUsersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserByUserTypesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetActiveUsersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetInactiveUsersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAuthorizedObjectsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ResetPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ChangePasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ChangeAuthorizedObjectTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
