6375d16ad7948b4748f73361eb0c2d4fed664cec
Michael Rath <40146720140057@litec.ac.at>
2018-08-28 10:32:41 +0200

    28.08.2018 10:32 final version 1.0

---
+++ FireApp_Service/obj/Release/Package/PackageTmp/bin/website/js/script.js
@@ -0,0 +1,9 @@
+/*
+function init() {
+    var cnt = document.getElementsByTagName("tr").length;
+    // console.log(cnt);
+    document.getElementById("cnt_rows").innerHTML = (cnt);
+}
+
+window.onload = init;
+*/
